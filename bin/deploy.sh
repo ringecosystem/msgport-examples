4ead03cd9e64743f5aafe6c46481a6874cc14221
bear <boundless.forest@outlook.com>
2024-06-19 16:22:23 +0800

    Delete the lib directory

--- bin/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-forge script script/Deploy.s.sol:DeploySender --broadcast --rpc-url https://koi-rpc.darwinia.network
-forge script script/Deploy.s.sol:DeployReceiver --broadcast --rpc-url https://ethereum-sepolia.publicnode.com
